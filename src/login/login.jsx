0a3a903eb9ef298db211d91af689d7380fe878b4
Kyle <bodenkyle8@gmail.com>
2024-11-04 12:59:26 -0700

    login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './login.css'; 
 
-export function Login() {
+export function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/userInfo'); // Navigates to the UserInfo component route
+  };
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main>
+      {/* <h1>Sign in to Apply Smart</h1> */}
+      <img
+        src="ApplySmartT.png"
+        style={{ width: '400px', height: 'auto' }}
+        className="responsive-image"
+        alt="Apply Smart logo"
+      />
+      <br />
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div id="username">
+          <input type="text" name="username" placeholder="username" />
+        </div>
+
+        <div id="password">
+          <input type="password" placeholder="password" />
+        </div>
+
+        <button type="button" onClick={handleNavigation}>Login</button>
+        <button type="button" onClick={handleNavigation}>Sign up!</button>
+      </form>
     </main>
   );
 }
