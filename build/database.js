f719fa5991b4c78f797783477db70403f2952dd3
Kyle <bodenkyle8@gmail.com>
2024-11-23 14:47:57 -0700

    idk why it isn't working

--- build/database.js
+++ build/database.js
@@ -4,8 +4,10 @@ const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
+const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000 });
+
 const db = client.db('startup');
+
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 const educationCollection = db.collection('education');
